6ac9daedc129f0cc34bbf27f39c6e1397aefe5b5
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-03-25 18:49:33 +0100

    seedData

--- schemas/user.js
+++ schemas/user.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-  const { Schema } = mongoose;
+const mongoose = require( 'mongoose' );
+const { Schema } = mongoose;
 
-  const userSchema = new Schema({
-    pseudo : {required : true, type : String} ,
-    pwd : {required : true, type : String}
-  }); 
-  
-const user = mongoose.model("User",userSchema)
+const userSchema = new Schema( {
+  pseudo: { required: true, type: String },
+  pwd: { required: true, type: String }
+} );
+
+const user = mongoose.model( "User", userSchema )
 
 
-export default user
+module.exports = user
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require( 'express' )
 const cors = require( 'cors' )
 const checkTokenMiddleware = require( './jsonwebtoken/check' )
 const bcrypt = require( 'bcrypt' )
-const mongonection =  require( './db.config' )
+const mongonection = require( './db.config' )
+const user = require( './schemas/user' )
 
 /************************************/
 /*** Import de la connexion à la DB */
@@ -38,12 +39,23 @@ app.get( '*', ( req, res ) => res.status( 501 ).send( 'What the hell are you doi
 
 
 mongonection
-    .then(() => console.log('Database connection OK'))
-    .then(() => {
-        app.listen(process.env.SERVER_PORT, () => {
-            console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
-        })
-    })
-    .catch(err => console.log('Database Error', err))
+    .then( () => console.log( 'Database connection OK' ) )
+    .then( async () => {
+        const users = await user.find()
+        console.log( users )
+        if ( users.length === 0 ) {
+            console.log( "Aucun user, user par défaut en création" )
+            const pwdHash = await bcrypt.hash( "nimda", 10 )
+            user.create( { pseudo: "admin", pwd: pwdHash }, ( err, user ) => {
+                if ( err ) console.log( err )
+            } )
+        }
+    } )
+    .then( () => {
+        app.listen( process.env.SERVER_PORT, () => {
+            console.log( `This server is running on port ${ process.env.SERVER_PORT }. Have fun !` )
+        } )
+    } )
+    .catch( err => console.log( 'Database Error', err ) )
 
 
