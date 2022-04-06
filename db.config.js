27dfd964ca5ff8f3f20eebcebca272ad1f938f46
dinoz0 <78912997+dinoz0@users.noreply.github.com>
2022-04-06 17:58:48 +0200

    correction route

--- db.config.js
+++ db.config.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 /************************************/
 /*** Connexion à la base de données */
 
-const mongonection = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tqs3y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+const mongonection = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.${process.env.DB_ID}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 
 
 module.exports = mongonection
