eb0c6f467d7204bed3bc52b4406fe5fa360a3b18
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-14 17:01:54 +0200

    refactor villa post

--- controller/villa.js
+++ controller/villa.js
@@ -1,18 +1,26 @@
 const axios = require( 'axios' )
+const { response } = require( 'express' )
 const article = axios.create( { baseURL: `http://${ process.env.API_ARTICLE }:${ process.env.API_PORT }` } )
-// const commercial = axios.create({baseURL: `http://${ process.env.API_COMMERCIAL }:${ process.env.API_PORT }`})
+const commercial = axios.create( { baseURL: `http://${ process.env.API_COMMERCIAL }:${ process.env.API_PORT }` } )
 exports.newVilla = async ( req, res ) => {
-    try {
-        const { nom, prix, image_article } = req.body
-        const response = await article.post( '/article', {
-            nom,
-            prix,
-            img: image_article
-        } )
-        console.log( `Villa ${ nom } créer` )
-        return res.status( response.status ).json( { ...response.data } )
-    } catch ( err ) {
+    let data
+    const { nom, prix, image_article, images_comm, description, pdf } = req.body
+    article.post( '/article', {
+        nom,
+        prix,
+        img: image_article
+    } ).then( ( response ) => data.article = { ...response.data } ).catch( ( err ) => {
         console.log( err );
-        return res.status( 500 ).json( { error: err } )
-    }
+        return res.status( 500 ).json( { error: "API Article down" } )
+    } )
+    commercial.post( '/commercial', {
+        imgs: [ ...images_comm ],
+        id: data.article._id,
+        description,
+        pdf
+    } ).then( ( response ) => data.commercial = { ...response.data } ).catch( ( err ) => {
+        console.log( err );
+        return res.status( 500 ).json( { error: "API Commercial down" } )
+    } )
+    return res.status( 201 ).json( { message: 'OK', data } )
 }
