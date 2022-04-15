e7a39b6917dc6e1edabbbae2938623c506a52b19
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-15 23:53:53 +0200

    fix limit payload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const schemaCommercial = require( './schemas/commercial' )
 const app = express()
 
 app.use( cors() )
-app.use( express.json() )
+app.use( express.json( { limit: "50mb" } ) )
 app.use( express.urlencoded( { extended: true } ) )
 
 /***********************************/
