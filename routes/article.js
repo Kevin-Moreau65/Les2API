3aa769bd2ad297c67616029ef52ab20ab8da6401
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-17 15:45:17 +0200

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -21,7 +21,7 @@ let router = express.Router()
 router.get( '/', articleCtrl.getAllArticle )
 router.get( '/:id', articleCtrl.getOneArticle )
 //admin
-router.post( '/', /*checkTokenMiddleware,*/ articleCtrl.newArticle )
-router.patch( '/:id', /*checkTokenMiddleware,*/ articleCtrl.updateArticle )
-router.delete( '/:id', /*checkTokenMiddleware,*/ articleCtrl.deleteArticle )
+router.post( '/', checkTokenMiddleware, articleCtrl.newArticle )
+router.patch( '/:id', checkTokenMiddleware, articleCtrl.updateArticle )
+router.delete( '/:id', checkTokenMiddleware, articleCtrl.deleteArticle )
 module.exports = router
