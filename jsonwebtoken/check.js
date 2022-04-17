6726112036f9352d951824d02512cb87af3834ad
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-17 15:58:30 +0200

    Update check.js

--- jsonwebtoken/check.js
+++ jsonwebtoken/check.js
@@ -1,41 +1,36 @@
 /***********************************/
 /*** Import des module nécessaires */
-const jwt = require('jsonwebtoken')
+const jwt = require( 'jsonwebtoken' )
 
 /*************************/
 /*** Extraction du token */
 const extractBearer = authorization => {
 
-    if(typeof authorization !== 'string'){
-        return false
+    if ( typeof authorization !== 'string' ) {
+        return undefined
     }
-
-    // On isole le token
-    const matches = authorization.match(/(bearer)\s+(\S+)/i)
-
-    return matches && matches[2]
-
+    return authorization.split( " " )[ 1 ]
 }
 
 
 /******************************************/
 /*** Vérification de la présence du token */
-const checkTokenMiddleware = (req, res, next) => {
+const checkTokenMiddleware = ( req, res, next ) => {
 
-    const token = req.headers.authorization && extractBearer(req.headers.authorization)
+    const token = req.headers.authorization && extractBearer( req.headers.authorization )
 
-    if(!token){
-        return res.status(401).json({ message: 'No token'})
+    if ( !token ) {
+        return res.status( 401 ).json( { message: 'No token' } )
     }
 
     // Vérifier la validité du token
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-        if(err){
-            return res.status(401).json({message: 'Bad token'})
+    jwt.verify( token, process.env.JWT_SECRET, ( err, decodedToken ) => {
+        if ( err ) {
+            return res.status( 401 ).json( { message: 'Bad token' } )
         }
 
         next()
-    })
+    } )
 }
 
 module.exports = checkTokenMiddleware
